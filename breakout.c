10ee9650fc3bfe491ab463a054b49e938a8bee02
Michael Salerno <michael.j.salerno@stonybrook.edu>
2014-04-18 22:08:33 -0400

    added color to breakout and fixed ball bouncing in wrong direction

--- breakout.c
+++ breakout.c
@@ -9,6 +9,7 @@
 void moveball();
 void updateball();
 void drawblocks(int rows);
+void colorsetup();
 
 int maxx, maxy;
 int bally = 0, ballx = 0, balld = 5;
@@ -20,6 +21,7 @@ int main() {
 
 	initscr();
 	clear();
+	colorsetup();
 	noecho();
 	cbreak();	/* Line buffering disabled. pass on everything */
 	curs_set(0);
@@ -28,17 +30,18 @@ int main() {
 	getmaxyx(stdscr, maxy, maxx);
 	bally = maxy/2;
 
-
+	attron(COLOR_PAIR(3));
 	mvprintw(maxy-1, 0, "=");
 	for (int i = 1; i < PADDEL_SIZE; ++i) {
 		printw("=");
 	}
+	attroff(COLOR_PAIR(3));
 
 	drawblocks(3);
 
 	refresh();
 
-	halfdelay(2);
+	halfdelay(1);
 
 	while(1) {
 		int c = getch();
@@ -48,7 +51,9 @@ int main() {
 			case KEY_LEFT:
 				if(padx > 0) {
 					padx--;
+					attron(COLOR_PAIR(3));
 					mvprintw(maxy-1, padx, "=");
+					attroff(COLOR_PAIR(3));
 					mvprintw(maxy-1, padx+PADDEL_SIZE, " ");
 				}
 				break;
@@ -56,7 +61,9 @@ int main() {
 				if(padx < (maxx - PADDEL_SIZE)) {
 					padx++;
 					mvprintw(maxy-1, padx-1, " ");
+					attron(COLOR_PAIR(3));
 					mvprintw(maxy-1, padx+PADDEL_SIZE-1, "=");
+					attroff(COLOR_PAIR(3));
 				}
 				break;
 			case -1:
@@ -99,7 +106,9 @@ void moveball() {
 			ballx--;
 			break;
 	}
-	mvprintw(bally, ballx, "o");
+	attron(COLOR_PAIR(2));
+	mvaddch(bally, ballx, 'o'| A_BOLD);
+	attroff(COLOR_PAIR(2));
 	mvprintw(maxy-2, 0, "MAX : y:%d x:%d", maxy, maxx);
 	mvprintw(maxy-1, 0, "BALL: y:%d x:%d", bally, ballx);
 	mvprintw(0, 0, "BLOCKS: %3d", blocks);
@@ -136,7 +145,7 @@ void updateball() {
 
 	char in = mvinch(y, x);
 	//mvprintw(maxy-1, 0, "READING: '%c'", in);
-	if((in != ' ' && in != 0) || (ballx < 0) || (bally < 0) || (ballx >= (maxx-1)) || (bally >= (maxy-1))) {
+	if((in != ' ' && in != 0) || (ballx < 1) || (bally < 0) || (ballx >= (maxx-1)) || (bally >= (maxy-1))) {
 		if(in == '#') {
 			mvprintw(y,x," ");
 			blocks--;
@@ -163,14 +172,14 @@ void updateball() {
 				balld = 11;
 				break;
 			case 7:
-				if(ballx < 0) {
+				if(ballx < 1) {
 					balld = 5;
 				} else {
 					balld = 11;
 				}
 				break;
 			case 11:
-				if(ballx < 0) {
+				if(ballx < 1) {
 					balld = 1;
 				} else {
 					balld = 7;
@@ -183,14 +192,29 @@ void updateball() {
 }
 
 void drawblocks(int rows) {
-
-	for (int i = 1; i < rows+1; ++i) {
-		move(i, 0);
+	int at=1;
+	attron(COLOR_PAIR(1));
+	for (int i = 0; i < rows; ++i) {
+		move(at, 0);
+		at += 4;
 
 		for (int j = 0; j < maxx; ++j) {
 			addch(BLOCK_SYM);
 			blocks++;
 		}
 	}
+	attroff(COLOR_PAIR(1));
 
+}
+
+void colorsetup() {
+	if(has_colors() == FALSE) {	
+		endwin();
+		printf("Your terminal does not support color\n");
+		exit(1);
+	}
+	start_color();
+	init_pair(1, COLOR_BLACK, COLOR_RED);        //BLOCK
+	init_pair(2, COLOR_WHITE, COLOR_BLACK);      //BALL
+	init_pair(3, COLOR_CYAN, COLOR_BLACK);       //PADDEL
 }
